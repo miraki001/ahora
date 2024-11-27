1998241468f57fbd67b1697bc7bd69b5474393a9
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-27 15:01:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import itables.options as it_op
+from itables.streamlit import interactive_table
 
 
 col1 = st.columns(1)
@@ -9,4 +11,6 @@ conn = st.connection("postgresql", type="sql")
 df1 = conn.query('select nuri,fuente,titulo,detalle from novedades order by nuri desc limit 100;', ttl="0"),
 with col1[0]:
   st.write(df1[0])
+
+  t = interactive_table(df )
 
