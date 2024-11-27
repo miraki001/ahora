ac36edb03f8a5c9017b686b53426ca5ecdcfd74f
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-27 14:46:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
 
 
 conn = st.connection("postgresql", type="sql")
