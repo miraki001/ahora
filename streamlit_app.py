492ad233cf02de295c3a675c5e6655c7921e71b0
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-27 14:48:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-col1 = st.columns((15.5, 14.5, 2), gap='medium')
+col1 = st.columns((1.5, 554.5, 2), gap='medium')
 
 conn = st.connection("postgresql", type="sql")
 df1 = conn.query('select nuri,fuente,titulo,detalle from novedades order by nuri desc limit 100;', ttl="0"),
