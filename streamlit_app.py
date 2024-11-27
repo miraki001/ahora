7ea4c9aa6825585ec0ca0508c35a2d6fd86c728c
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-11-27 14:02:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+conn = st.connection("postgresql", type="sql")
+df1 = conn.query('select nuri,fuente,titulo,detalle order by nuri desc limit 100;', ttl="0"),
+
+event = st.dataframe(
+    df1,
+    key="data",
+    on_select="rerun",
+    selection_mode=["multi-row", "multi-column"],
+)
+
+event.selection
