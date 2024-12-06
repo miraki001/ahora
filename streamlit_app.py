21dd36216d20f89b9b4db4646f58a573928a64b9
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-06 14:13:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,13 @@ import wordcloud
 import itables.options as it_op
 from itables.streamlit import interactive_table
 import matplotlib.pyplot as plt
+import re
 #from wordcloud import WordCloud
 from nltk.corpus import stopwords
 from nltk.stem.porter import * 
 from gensim.models import word2vec 
 from sklearn.manifold import TSNE 
+from nltk.tokenize import word_tokenize
 stemmer = PorterStemmer()  
 
 
