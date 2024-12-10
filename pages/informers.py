f8a03d2e8b9eb980a8402df8dadda2269b0b9528
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-10 16:15:30 -0300

    Update informers.py

--- pages/informers.py
+++ pages/informers.py
@@ -23,3 +23,63 @@ st.set_page_config(
 st.title("Miraki")
 st.subheader("Plataforma de Vigilancia Tecnólogica e Inteligencia Competitiva")
 st.image("ic_launcher44.png", width=100,)
+
+conn = st.connection("postgresql", type="sql")
+df1 = conn.query('select nuri,fuente,select_web selec,fecha,titulo,detalle,imagen,link from novedades order by nuri desc limit 2000;', ttl="0"),
+df = df1[0]
+#st.write(df1[0])
+
+
+df = df[pd.notnull(df['titulo'])]
+
+df['fecha'] = pd.to_datetime(df.fecha, format='%Y-%m-%d')
+fig = plt.figure()
+ax = df.groupby(df.fecha.dt.year)['titulo'].count().plot(kind='bar', figsize=(12, 6))
+ax.set(xlabel='Año', ylabel='Number of Articles', title="Articles Published Every Year")
+plt.show()
+st.pyplot(fig)
+
+fig1 = plt.figure()
+ax = df.groupby(df.fecha.dt.month)['titulo'].count().plot(kind='bar')
+months = ['JAN',  'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+#ax.set_xticklabels(months)
+ax.set(xlabel='Month', ylabel='Number of Articles', title="Articles Published Every Month")
+plt.show()
+st.pyplot(fig1)
+
+fig2 = plt.figure()
+nltk.download('stopwords')
+stop_words = stopwords.words('english')
+stopword_es = nltk.corpus.stopwords.words('spanish')
+stop_words = stop_words + stopword_es
+
+def cleaning(df, stop_words):
+    df['titulo'] = df['titulo'].apply(lambda x:' '.join(x.lower() for x in x.split()))
+    # Replacing the digits/numbers
+    df['titulo'] = df['titulo'].str.replace('^\d+\s|\s\d+\s|\s\d+$', '')
+    # Removing stop words
+    df['titulo'] = df['titulo'].apply(lambda x:' '.join(x for x in x.split() if x not in stop_words))
+    # Lemmatization
+#    df['titulo'] = df['titulo'].apply(lambda x:' '.join([Word(x).lemmatize() for x in x.split()]))
+    return df
+
+data_v1 = cleaning(df, stop_words)
+data_v1.head()
+
+
+# Create and generate a word cloud image:
+#wordcloud = WordCloud().generate(text)
+
+common_words=''
+for i in data_v1.titulo:  
+    i = str(i)
+    tokens = i.split()
+    common_words += " ".join(tokens)+" "
+wordcloud = wordcloud.WordCloud().generate(common_words)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+st.pyplot()
+
+
+
