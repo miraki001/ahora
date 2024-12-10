b05019250fc04a13e32b4f74479a859bb0e92ca6
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-10 15:48:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.set_page_config(
     initial_sidebar_state="expanded")
 st.title("Miraki")
 st.subheader("Plataforma de Vigilancia Tecnólogica e Inteligencia Competitiva")
-st.image("ic_launcher44.png")
+st.image("ic_launcher44.png",, width=200,)
 
 #default=["copyHtml5", "csvHtml5", "excelHtml5", "colvis"],
 default=["copyHtml5", "csvHtml5", "excelHtml5"],
