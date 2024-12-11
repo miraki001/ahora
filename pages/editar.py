b811398400042a620d8e02f599dee5fe4d041966
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-11 13:01:58 -0300

    Update editar.py

--- pages/editar.py
+++ pages/editar.py
@@ -4,5 +4,17 @@ from streamlit_navigation_bar import st_navbar
 import pages as pg
 
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
 st.write(page)
