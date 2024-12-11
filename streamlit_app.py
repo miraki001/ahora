9ca103eeeb1145272f557dc036a3729da8bf71a8
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-11 14:00:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,27 @@ st.set_page_config(initial_sidebar_state="expanded")
 
 
 
+
+# define what option labels and icons to display
+option_data = [
+   {'icon': "bi bi-hand-thumbs-up", 'label':"Agree"},
+   {'icon':"fa fa-question-circle",'label':"Unsure"},
+   {'icon': "bi bi-hand-thumbs-down", 'label':"Disagree"},
+]
+
+# override the theme, else it will use the Streamlit applied theme
+over_theme = {'txc_inactive': 'white','menu_background':'purple','txc_active':'yellow','option_active':'blue'}
+font_fmt = {'font-class':'h2','font-size':'150%'}
+
+# display a horizontal version of the option bar
+op = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
+
+# display a version version of the option bar
+op2 = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=False)
+
+
+
+
 #default=["copyHtml5", "csvHtml5", "excelHtml5", "colvis"],
 #default=["copyHtml5", "csvHtml5", "excelHtml5"],
 
