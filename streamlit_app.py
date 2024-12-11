d2855093cd16e0d8b9f1c57e9de1e76cc31644ea
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-11 15:03:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if col1.button("Home"):
 if col2.button("Page 1"):
     st.switch_page("./pages/editar.py")
 if col3.button("Page 2"):
-    st.switch_page("./pages/seleccionar.py"
+    st.switch_page("./pages/seleccionar.py")
 
 #default=["copyHtml5", "csvHtml5", "excelHtml5", "colvis"],
 #default=["copyHtml5", "csvHtml5", "excelHtml5"],
