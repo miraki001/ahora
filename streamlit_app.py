34753aef5f12f0e78e9d1f21950403c9bef21594
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-11 12:23:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ from streamlit_navigation_bar import st_navbar
 
 st.set_page_config(initial_sidebar_state="collapsed")
 
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+page = st_navbar(["Home", "informes", "Examples", "Community", "About"])
 st.write(page)
 
 
