bd127214b00ad2cf2f56ed36619cca70ccdf2872
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-11 15:30:04 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -39,7 +39,26 @@ if col5.button("Informes"):
 st.subheader("Plataforma de Vigilancia Tecnólogica e Inteligencia Competitiva")
 st.image("ic_launcher44.png", width=100,)
 
+modal = Modal(
+    "Demo Modal", 
+    key="demo-modal",
+    
+    # Optional
+    padding=20,    # default value
+    max_width=744  # default value
+)
 
+open_modal = st.button("Open")
+if open_modal:
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
+        st.write(f"Checkbox checked: {value}")
 
 
 conn = st.connection("postgresql", type="sql")
