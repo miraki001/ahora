e627178645487221af64652b88059db71116f868
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-11 13:08:57 -0300

    Update editar.py

--- pages/editar.py
+++ pages/editar.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
-import pages as pg
-
-
 
 page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
 st.write(page)
