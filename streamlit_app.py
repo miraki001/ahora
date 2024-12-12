729187859ee2dfb584ff30f07450461e0eb574fc
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-12 15:46:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,8 @@ config = {
 }
 result = st.data_editor(df, column_config = config, num_rows='dynamic')
 #edited_df = st.data_editor(df) # 👈 An editable dataframe
-selected_indices = st.multiselect('Select rows:', df.nuri)
-selected_rows = df.loc[selected_indices]
+#selected_indices = st.multiselect('Select rows:', df.nuri)
+selected_rows = df.loc['nuri']
 
 # Display the selected data
 st.write('Selected Rows:')
