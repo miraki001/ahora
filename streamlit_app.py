b09dac1e24490e9ab17bcc4263720bd9291b7a53
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 14:56:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,7 @@ selection = dataframe_with_selections(df)
 st.write("Your selection:")
 st.write(selection)
 st.write(f'selected row index: {selection.selected_row_index}')
-st.write(f'car name: {selection.df.at[selection.selected_row_index, "nuri]}')
+st.write(f'car name: {selection.df.at[selection.selected_row_index, "nuri"]}')
 #st.write(selection[0])
 st.write(selection['nuri'])
 vnuri= selection['nuri']
