ea96dfe62fc36bbc2ca22cd5c2be3141299403df
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 15:08:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,6 +95,7 @@ def dataframe_with_selections(df):
 selection = dataframe_with_selections(df)
 
 st.dataframe(selection, use_container_width=False)
+st.dataframe(selection, hide_index=True)
 st.write("Your selection:")
 st.write(selection)
 #st.write(f'selected row index: {selection.selected_row_index}')
