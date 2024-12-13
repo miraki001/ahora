9f7d6b0234b529e14d343cfefe5cea2e50033379
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 13:52:49 -0300

    Update seleccionar.py

--- pages/seleccionar.py
+++ pages/seleccionar.py
@@ -1,1 +1,6 @@
 import streamlit as st
+
+
+conn = st.connection("postgresql", type="sql")
+actualizar = 'update novedades set select_web = ' + 'S' + ' ,nro_reporte = 0 where nuri = ' + vnuri + ';'
+df1 = conn.query(actualizar, ttl="0"),
