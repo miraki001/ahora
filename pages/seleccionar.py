3088a441318100dd01657ae2b7986c2cd6464900
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 17:00:53 -0300

    Update seleccionar.py

--- pages/seleccionar.py
+++ pages/seleccionar.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import psycopg2
 
 tnuri = st.session_state['vnuri']
 st.write(tnuri)
@@ -8,7 +9,7 @@ actualizar = 'update novedades set select_web = :estado,nro_reporte = 0 where nu
 st.write(actualizar)
 
 cursor = conn.cursor()
-cursor.execute(actualizar)
+cursor.execute(actualizar, "S")
 
 
 #df1 = conn.execute(actualizar, ttl="0",params={"estado": "S"} ),
