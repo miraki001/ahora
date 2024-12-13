e912124419a4ec0189b9fa26e145263aba4763ce
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 13:53:41 -0300

    Update seleccionar.py

--- pages/seleccionar.py
+++ pages/seleccionar.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-
+tnuri = st.session_state['vnuri']
 conn = st.connection("postgresql", type="sql")
-actualizar = 'update novedades set select_web = ' + 'S' + ' ,nro_reporte = 0 where nuri = ' + vnuri + ';'
+actualizar = 'update novedades set select_web = ' + 'S' + ' ,nro_reporte = 0 where nuri = ' + tnuri + ';'
 df1 = conn.query(actualizar, ttl="0"),
