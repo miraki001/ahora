78ff6f27080b2e6195f98d192178a05904a30f8f
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 14:30:44 -0300

    Update editar.py

--- pages/editar.py
+++ pages/editar.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-vtitulo = st.session_state['vtitulo']
+#vtitulo = st.session_state['vtitulo']
 vtitulo = "eeeeee"
 st.write(vtitulo)
 col1, col2, col3,col4,col5 = st.columns(5)
