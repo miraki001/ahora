acd671747aba3bd62d0f8b94a63baec92cadc177
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 17:04:30 -0300

    Update seleccionar.py

--- pages/seleccionar.py
+++ pages/seleccionar.py
@@ -4,12 +4,23 @@ import psycopg2
 tnuri = st.session_state['vnuri']
 st.write(tnuri)
 conn = st.connection("postgresql", type="sql")
-actualizar = 'update novedades set select_web = :estado,nro_reporte = 0 where nuri = ' + tnuri + ';'
 
+actualizar = 'update novedades set select_web = :estado,nro_reporte = 0 where nuri = ' + tnuri + ';'
+nuri = tnuri
+new = 'S'
 st.write(actualizar)
 
-cursor = conn.cursor()
-cursor.execute(actualizar, "S")
+try:
+    conn.query(f"UPDATE novedades SET select_web = '{new}' WHERE nuri='{nuri}'")
+
+    conn.commit()
+except Exception as e:
+    print(e)
+finally:
+    st.write("Yessss!!")
+
+#cursor = conn.cursor()
+#cursor.execute(actualizar, "S")
 
 
 #df1 = conn.execute(actualizar, ttl="0",params={"estado": "S"} ),
