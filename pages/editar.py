a5695ef9ee2658b733036be0ba38984ceb8f61f9
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 12:44:07 -0300

    Update editar.py

--- pages/editar.py
+++ pages/editar.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.write("QueryParams: ", st.query_params)
-value  = int(st.query_params.get("nuri", vnuri))
-st.write(value)
+#value  = int(st.query_params.get("nuri", vnuri))
+#st.write(value)
