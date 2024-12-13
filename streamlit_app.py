9722cf3e2ab060ffb2594fc36f7407710747a6da
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 13:22:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ if 'user_select_value' not in st.session_state:
 user_select_value = st.session_state['user_select_value']
 
 if st.button('Save Filters'):
-        st.session_state['user_select_value'] = user_select_value
+        st.session_state['user_select_value'] = 500
 
 if st.button('Clear page Filters'):
         st.session_state['user_select_value'] = 0 # or default value
