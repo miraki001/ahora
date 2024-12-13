0e6e42ed1eb7c72802f430d6cf4fe7f3f374cd8f
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-13 13:44:18 -0300

    Update editar.py

--- pages/editar.py
+++ pages/editar.py
@@ -1,10 +1,21 @@
 import streamlit as st
 
-
 st.set_page_config(layout="wide")
 
-st.write("QueryParams: ", st.query_params)
-st.write('session')
-st.write(st.session_state)
+if col1.button("Home"):
+    st.switch_page("streamlit_app.py")
+if col2.button("Editar"):
+    st.switch_page("./pages/editar.py")
+if col3.button("Seleccionar"):
+    st.switch_page("./pages/seleccionar.py")
+if col4.button("Desmarcar"):
+    st.switch_page("./pages/seleccionar.py")
+if col5.button("Informes"):
+    st.switch_page("./pages/informes.py")
+
+#st.write("QueryParams: ", st.query_params)
+#st.write('session')
+#st.write(st.session_state)
 #value  = int(st.query_params.get("nuri", vnuri))
 st.write(st.session_state['vnuri'])
+st.write(st.session_state['titulo'])
