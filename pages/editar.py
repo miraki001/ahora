8e7efced00e0a616e7c3d2de883c5e19c462b027
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-16 17:50:12 -0300

    Update editar.py

--- pages/editar.py
+++ pages/editar.py
@@ -33,7 +33,7 @@ vtitle = st.text_input("Titulo", ttitulo)
 vtitle_es = st.text_input("Titulo en Castellano ", st.session_state['vtitulo_es'])
 
 vdet= st.text_input("Destalle", st.session_state['vdetalle'])
-vdet_es = st.text_input("Detalle en Castellano ", st.session_state['vdetalle_es'])
+vdet_es = st.text_input(":red[Detalle en Castellano] ", st.session_state['vdetalle_es'])
 
 vlink = st.text_input("Link ", st.session_state['vlink'])
 vimg = st.text_input("Imagen ", st.session_state['vimagen'])
