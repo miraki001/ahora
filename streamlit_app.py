380ffdd32ff989cd37ff9b91d972ffca40364d71
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-16 14:53:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ if col5.button("Informes"):
 
 
 conn = st.connection("postgresql", type="sql")
-df1 = conn.query('select nuri,fuente,fecha,titulo,detalle,imagen,link from novedades order by nuri desc limit 50;', ttl="0"),
+df1 = conn.query('select nuri,fuente,fecha,titulo,select_web,detalle,imagen,link from novedades order by nuri desc limit 50;', ttl="0"),
 df = df1[0]
 #st.write(df1[0])
 
