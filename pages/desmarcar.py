ca054571b6df4351af14b9ef45c256f6136a2d49
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-16 14:48:02 -0300

    Update desmarcar.py

--- pages/desmarcar.py
+++ pages/desmarcar.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import psycopg2
+from sqlalchemy import text
+
+tnuri = st.session_state['vnuri']
+st.write(tnuri)
+conn = st.connection("postgresql", type="sql")
+
+actualizar = 'update novedades set select_web = :estado,nro_reporte = 0 where nuri = ' + tnuri + ';'
+nuri = tnuri
+new = 'N'
+st.write(actualizar)
+
+
+with conn.session as session:
+    #session.execute(f"UPDATE novedades SET select_web = '{new}' WHERE nuri='{nuri}'")
+    session.execute(text("UPDATE novedades SET select_web = :val WHERE nuri= :nuri"), {"val": new,"nuri": nuri})
+                        
+    session.commit()
+    st.success("Data sent")
 
