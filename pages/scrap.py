791e4a90f032288bfd83d32f8657e7954d2cfd89
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-17 14:55:27 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -5,15 +5,20 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-#@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+options = Options() 
+options.add_argument("--headless=new")
+options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(options=options)
 
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
-driver = get_driver()
+#driver = get_driver()
 driver.get('http://example.com')
 
 st.code(driver.page_source)
