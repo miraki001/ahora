aba14b065ed8e406a4d3523c83fa4427a39e46ff
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-17 14:41:51 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
+#@st.experimental_singleton
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
