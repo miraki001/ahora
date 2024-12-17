345a262d6d5dbabc1d3c790d3cbc0cb73a3b9da6
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-17 15:33:12 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -1,23 +1,20 @@
 import streamlit as st
-
-
-import os, sys
-
-
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from os.path import exists
 
-browser.get('http://example.com')
-st.write(browser.page_source)
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+
+driver = get_driver()
 driver.get('http://example.com')
 
 st.code(driver.page_source)
