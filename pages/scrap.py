3353dbc6bf4f264b0853f6f679907dffdd39c852
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-17 15:00:10 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -2,8 +2,8 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ECfrom             
-selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
