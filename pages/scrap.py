ac1ca33baaf5b617c7adaaf5049ce40f96242d4c
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-19 10:54:57 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -56,7 +56,7 @@ all_img = driver.find_elements(By.XPATH, '//h1')
 #st.write(all_img)
 
 titulo = driver.find_elements(By.XPATH, '//div[@class="ob-card-body"]')
-link = driver.find_elements(By.XPATH, '//div[@class="ob-card-header"]/a')
+link = driver.find_elements(By.XPATH, '//div[@class="ob-card-footer"]/a')
 #link = driver.find_elements(By.XPATH, '//*[@id="main"]/div/section[1]/div/div[3]/div/div[1]/div[2]/a')
 st.write(link)
 element_list = [] 
