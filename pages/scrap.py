21882f236a47b12c5f4be31ca48b8ffb51ad0727
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-20 17:03:15 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -154,7 +154,7 @@ sepa = '//article[@class="col-xl-3 col-md-6 mb-15 mt-15 wow fadeIn animated"]'
 noticias = driver.find_elements(By.XPATH, sepa)
 st.write(noticias)
 for noticias in noticias:
-    name = noticias.find_element(By.XPATH, './/h6[@class="post-title font-weight-bold mb-10"]').text
+    name = noticias.find_element(By.XPATH, './/h6').text
     img = noticias.find_element(By.XPATH, ".//img").get_attribute("src")
     link = noticias.find_element(By.XPATH, ".//a").get_attribute("href")
     det = noticias.find_element(By.XPATH, './/p').get_attribute("text")
