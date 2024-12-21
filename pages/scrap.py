eeb5adb7bdcf4c46f1d0700034eb171812b0620a
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-21 11:31:34 -0300

    Update scrap.py

--- pages/scrap.py
+++ pages/scrap.py
@@ -28,6 +28,11 @@ def get_driver():
     options.add_argument('--headless')
     #options.add_argument(f"--window-size={width}x{height}")
     options.add_argument(f"--user-agent={my_user_agent}")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
     
     service = Service()
     driver = webdriver.Chrome(service=service, options=options)
