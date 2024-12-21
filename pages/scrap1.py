f527bdf9cc259a28adc4ce3ab441ac96015c09ba
miraki <derquicia@mariogoldsteinsa.com.ar>
2024-12-21 10:03:09 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -26,7 +26,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 response = requests.get('https://www.vinetur.com/marketing/')
 soup = BeautifulSoup(response.text, 'html.parser')
-data = soup.find('div').text
+#data = soup.find('div').text
+data = soup.find_all(string="background")
 st.write(data)
 st.write('despues')
 
