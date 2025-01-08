4e19b0fc7da2f4c6dd5210bc923ffdb1fd088cbd
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-08 12:57:00 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -31,7 +31,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 #data = soup.find('div').text
 
 
-products = driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/ul/li")
+products = driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/ul")
 st.write(products)
 data = soup.find_all('a', 'li')
 
