fe5c37d3a83375242862bbd6d035819d3f3a7383
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-08 13:18:25 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -79,7 +79,9 @@ datos = driver.find_elements(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/ul/l
 st.write(datos)
 for datos in datos:
     name = datos.find_element(By.XPATH, './/a').text
+    link = datos.find_element(By.XPATH, ".//a").get_attribute("href")
     st.write(name)
+    st.write(link)
 
     
 
