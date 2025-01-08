ee94408e837bfdccaa255cc23703b65a3e1be7fb
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-08 13:07:05 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import csv
 import subprocess
 import sys
