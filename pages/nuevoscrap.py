5c122ef387d9bee1cc3d4d02744f014cc0cabfc5
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-10 13:55:56 -0300

    Update nuevoscrap.py

--- pages/nuevoscrap.py
+++ pages/nuevoscrap.py
@@ -43,9 +43,6 @@ def clean_content(body_content):
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL")
 
-if st.button("Scrape Site"):
-    st.write("Scraping the website...")
-    # Further logic will go here
 
 if st.button("Scrape Site"):
     raw_html = scrape_website(url)
