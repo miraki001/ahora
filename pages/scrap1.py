cfe18facbbf014636b2eedd0c1339ea9c1e6282e
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-12 12:43:09 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -86,7 +86,7 @@ for datos in datos:
 #    try:
         name = datos.find_element(By.XPATH, ".//a").text
         link = datos.find_element(By.XPATH, ".//a").get_attribute("href")
-        img = datos.find_element(By.XPATH, "./li").value_of_css_property("background")
+        img = datos.find_element(By.XPATH, "li").value_of_css_property("background")
 #        image_url = getCssValue('background')
 #        img = datos.find_elements(By.CSS_SELECTOR, "background").value_of_css_property("background")
 #        soup = BeautifulSoup(datos)
