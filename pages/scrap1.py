af84c1d88775062e5b1d9c7d7d53bba404ea00d5
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-13 13:55:55 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -111,7 +111,7 @@ st.write('hasta aca')
 
 
 driver.get('https://enolife.com.ar/es/category/fincas/')
-datos = driver.find_elements(By.XPATH, ".//ul/li")
+datos = driver.find_elements(By.XPATH, "//*[@class='item']")
 st.write(datos)   
 for datos in datos:
         #name = datos.find_element(By.XPATH, ".//h2/a").text
