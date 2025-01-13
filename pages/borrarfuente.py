9c04c4d094eda0f0c443e6b71c3592015964ce2d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-13 15:46:15 -0300

    Update borrarfuente.py

--- pages/borrarfuente.py
+++ pages/borrarfuente.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import psycopg2
+from sqlalchemy import text
 
+st.set_page_config(layout="wide")
+conn = st.connection("postgresql", type="sql")
+
+tnuri = st.session_state['vnuri']
