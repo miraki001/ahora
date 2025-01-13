16e65e37dffea70333ba5ee0af17e898470218c0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-13 13:19:51 -0300

    Update verifica_pag.py

--- pages/verifica_pag.py
+++ pages/verifica_pag.py
@@ -90,7 +90,7 @@ noticias = driver.find_elements(By.XPATH, separador)
 #st.write(noticias)
 for noticias in noticias:
     #name = noticias.find_element(By.XPATH, xtitulo).text
-    name = noticias.find_element(By.XPATH, xtitulo).get_attribute("text")
+    name = noticias.find_element(By.XPATH, xtitulo).text
 
     #img = noticias.find_element(By.XPATH, ximage).get_attribute("src")
     link = noticias.find_element(By.XPATH, xlink).get_attribute("href")
