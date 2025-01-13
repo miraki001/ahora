dce69062ec49e1e5ac845d6da1766d8a558538f3
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-13 11:36:18 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -54,14 +54,14 @@ if st.button("Aplicar"):
        pactiva = 'S'
     else:
         pactiva = 'N'
-    df[(df.activa == pactiva) & (df.descrip == pfuente)]
+    df[(df.activa == pactiva) & (df.fuente == pfuente)]
     st.write(pactiva)
     st.write(pfuente)
     #qq = 'select nuri,fuente as url,activa,fecha_act,descrip as fuente,pais from fuentes_py where proyecto_nuri = 1 and activa = :pactiva  and descrip = :pfuente  ;'
     #df5 = conn.query(qq, ttl="0",params={"pactiva": pactiva, "pfuente": pfuente} )
     #df1 = conn.query(qq, ttl="0")
-    st.write(df5)    
-    df = df5[0]
+    #st.write(df5)    
+    #df = df5[0]
 
 #st.write(df1[0])
 #st.dataframe(df, hide_index=True, column_config={"titulo_es": None})
