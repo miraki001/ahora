51bff27f8feb5d9f7ce0e6d2dbbabca86e83e99b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 14:51:17 -0300

    Update scraptodo.py

--- pages/scraptodo.py
+++ pages/scraptodo.py
@@ -116,7 +116,7 @@ def scrap():
         snuri = str(tnuri)
         conn = st.connection("postgresql", type="sql")
 # vamos a bucar si ya existe la noticias
-        vquery = "select count(nuri) as cnt from novedades where proyecto_nuri = 1 and  fuente_nuri = " + snuri + " and titulo =  '" + ptitulo +  "'  ;"
+        vquery = "select count(nuri) as cnt from novedades where proyecto_nuri = 1 and  fuente_nuri = " + snuri + " and titulo =  " + ptitulo +  "  ;"
         df2 = conn.query(vquery, ttl="0")
         cnt = dfd.to_string(columns=['cnt'], header=False, index=False)
         pcnt = int(cnt)
