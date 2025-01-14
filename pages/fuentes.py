2a1556dc557d961fa718acdb2b7b662acd261dea
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 12:10:53 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -21,7 +21,8 @@ vtitulo= ''
 vdetalle = ''
 vlink = ''
 vimagen = ''
-
+button1_color = "#00FF00"  # Green color for Button 1
+button2_color = "#FF0000"  # Red color for Button 2
 
 #st.query_params.from_dict({"foo": "bar", "baz": [1, 2, 3]})
 
