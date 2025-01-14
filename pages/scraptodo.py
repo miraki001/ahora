458f7177fabcee779cbd25dd97368f14ec5b7fcd
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 08:32:51 -0300

    Update scraptodo.py

--- pages/scraptodo.py
+++ pages/scraptodo.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import os
+import sys
+import psutil
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from os.path import exists
+import csv
+from time import sleep
+import pandas as pd
+import re
+from streamlit_extras.image_in_tables import table_with_images
+import streamlit.components.v1 as components
+from streamlit_echarts import st_echarts
+import base64
 
