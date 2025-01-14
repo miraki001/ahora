4acdb0e473e17d87f3522d5d930675354ce1c693
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 11:49:43 -0300

    Update scraptodo.py

--- pages/scraptodo.py
+++ pages/scraptodo.py
@@ -28,6 +28,10 @@ vimag = ''
 vnuri = 0
 vfuente = ''
 vurl = ''
+titulo = ''
+det = ''
+link = ''
+img = ''
 
 
 my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
