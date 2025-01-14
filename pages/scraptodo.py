9e3c75501859570004bd22784a5112fb024539a2
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 14:05:00 -0300

    Update scraptodo.py

--- pages/scraptodo.py
+++ pages/scraptodo.py
@@ -104,28 +104,21 @@ def scrap():
             pimg = img
         link = noticias.find_element(By.XPATH, vlink).get_attribute("href")
         detalle = noticias.find_element(By.XPATH, vdeta).text
-        #link1 = noticias.find_element(By.XPATH, ".//a/following::a").get_attribute("href")
-        st.write(titulo)
-        #st.write(img)
-        st.write(link)
+        #st.write(titulo)
+        #st.write(link)
         ptitulo = titulo
         pdet = detalle
         plink = link
-        st.write('titulo1')
+        #st.write('titulo1')
         st.write(ptitulo)
         conn = st.connection("postgresql", type="sql")
-        #qq = "select nextval('prueba_seq') as nuri  ;"
-        #df5 = conn.query(qq, ttl="0")
-        #st.write(df5)
-        #vnuri = df5.to_string(columns=['nuri'], header=False, index=False)
-        #st.write(vnuri)
         with conn.session as session:
-#            ingresar = "insert into novedades (nuri,fuente,titulo,detalle,link,tipo,imagen,fecha,proyecto_nuri,fuente_nuri,eje_nuri)"
-#            ingresar = ingresar + " values (nextval('novedades_seq'),:fuente,:titulo,:detalle,:link,'P',:imagen,current_date,1,:fuente_nuri,1); "
-#            session.execute(text(ingresar), {"fuente": vfuente,"titulo": ptitulo,"detalle": pdet,"link": plink, "imagen": pimg,"fuente_nuri": vnuri})
-            ingresar = "insert into prueba (nuri, otro) "
-            ingresar = ingresar + "values ( nextval('prueba_seq') , :valor)  ;"
-            session.execute(text(ingresar),{"valor": 1})
+            ingresar = "insert into novedades (nuri,fuente,titulo,detalle,link,tipo,imagen,fecha,proyecto_nuri,fuente_nuri,eje_nuri)"
+            ingresar = ingresar + " values (nextval('novedades_seq'),:fuente,:titulo,:detalle,:link,'P',:imagen,current_date,1,:fuente_nuri,1); "
+            session.execute(text(ingresar), {"fuente": vfuente,"titulo": ptitulo,"detalle": pdet,"link": plink, "imagen": pimg,"fuente_nuri": vnuri})
+            #ingresar = "insert into prueba (nuri, otro) "
+            #ingresar = ingresar + "values ( nextval('prueba_seq') , :valor)  ;"
+            #session.execute(text(ingresar),{"valor": 1})
             session.commit()
 
 
