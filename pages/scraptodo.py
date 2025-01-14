6202faeeee67488b42819a2b9de8bc2fdff66847
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 10:25:01 -0300

    Update scraptodo.py

--- pages/scraptodo.py
+++ pages/scraptodo.py
@@ -20,7 +20,10 @@ import base64
 import psycopg2
 from sqlalchemy import text
 
-def scrap
+my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+
+
+def scrap()
 
 
 
