a96c7292d6cda08675beec8a2e65af770e36f333
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-14 08:39:28 -0300

    Update scraptodo.py

--- pages/scraptodo.py
+++ pages/scraptodo.py
@@ -26,3 +26,6 @@ qq = "select * from fuentes_py where proyecto_nuri = 1 and activa = 'S'  ;"
 df1 = conn.query(qq, ttl="0"),
 df = df1[0]
 st.write(df)
+
+for i in range(len(df)):
+  st.write(df[i])
