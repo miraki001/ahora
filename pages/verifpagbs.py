38d8079a31b5d7063c635382d185d8b5f5354bc6
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-15 14:46:55 -0300

    Update verifpagbs.py

--- pages/verifpagbs.py
+++ pages/verifpagbs.py
@@ -1,1 +1,27 @@
+import streamlit as st
+import os
+import sys
+import psutil
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
 
+
+
+url = 'https://www.thedrinksbusiness.com/tag/wine/'
+response = requests.get(url)
+html_content = response.content
+tree = html.fromstring(html_content)
+soup = BeautifulSoup(html_content, 'lxml')
+
+noticias = soup.find_all('div', class_='col-md-4 mb-4')
+for p in noticias:
+    title = p.find('a')
+    href = title.get("href")
+    tite = p.find('h2').get_text()
+    det = p.find('h2').get_text()
+
+    #p.attrs['a']
+    print(href)
+    print(tite)
+    print(det)
