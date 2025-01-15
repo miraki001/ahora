df9cda2288c6eedcdb96d0978b55b62014bb27bd
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-15 11:36:29 -0300

    Update scrap1.py

--- pages/scrap1.py
+++ pages/scrap1.py
@@ -90,7 +90,7 @@ for datos in datos:
 #        img = datos.find_element(By.XPATH, "li").value_of_css_property("background")
         
         img = datos.value_of_css_property("background")
-        det = datos.find_element(By.XPATH, ".//div[@class='post-content']/p").et_attribute("text")
+        det = datos.find_element(By.XPATH, ".//div[@class='post-content']/p").get_attribute("text")
         st.write(img)
 #        img = re.split('[()]',img)[3]
 #        image_url = getCssValue('background')
