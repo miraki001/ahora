ca27fcf14075210da1fed522bd5baf355c856b78
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-15 12:11:20 -0300

    Update palabrasclaves.py

--- pages/palabrasclaves.py
+++ pages/palabrasclaves.py
@@ -3,48 +3,7 @@ import psycopg2
 from sqlalchemy import text
 from streamlit_extras.stylable_container import stylable_container
 
-"""
-st.set_page_config(page_title='First app', page_icon="📊", initial_sidebar_state="expanded", layout='wide')
-from streamlit.components.v1 import html
-st.sidebar.text("menu options")
-html("""<script>
-        var decoration = window.parent.document.querySelectorAll('[data-testid="stDecoration"]')[0];
-        var isVisible = true;
 
-        function toggleVisibility() {
-            isVisible = !isVisible;
-            //decoration.style.display = isVisible ? "flex" : "none";
-            decoration.innerText = isVisible ? "Welcome, Streamlit App!" : "";
-        }
-
-        // Locate elements
-        var sidebar = window.parent.document.querySelectorAll('[data-testid="stSidebar"]')[0];
-
-        // Observe sidebar size
-        function outputsize() {
-            decoration.style.left = `${sidebar.offsetWidth}px`;
-        }
-
-        new ResizeObserver(outputsize).observe(sidebar);
-
-        // Adjust sizes
-        outputsize();
-        decoration.style.height = "3.0rem";
-        decoration.style.right = "45px";
-
-        // Adjust text decorations
-        decoration.innerText = "Miraki - Plataforma de Vigilancia Tecnologica e Inteligencia Competitiva";
-        decoration.style.fontWeight = "bold";
-        decoration.style.display = "flex";
-        decoration.style.justifyContent = "center";
-        decoration.style.alignItems = "center";
-
-        // Add click event listener
-        decoration.addEventListener("click", toggleVisibility);
-    </script>
-""", width=0, height=0)
-
-"""
 
 #col41, mid, col42 = st.columns([1,1,20])
 #with col41:
