f31a900e141c334b724730036d0136df206460bf
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-15 16:24:59 -0300

    Update verifpagbs.py

--- pages/verifpagbs.py
+++ pages/verifpagbs.py
@@ -43,7 +43,7 @@ soup = BeautifulSoup(html_content, 'lxml')
 st.write('aca')
 
 #noticias = soup.find_all(class_='col-md-4 mb-4')
-noticias = soup.find_all(re.compile(separador))
+noticias = soup.find_all(attrs=separador)
 for p in noticias:
     title = p.find(xlink)
     href = title.get("href")
