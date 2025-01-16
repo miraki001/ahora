ed06a0650cd3371057976ec88e380f99fee80e1e
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-16 10:59:22 -0300

    Update ingpalabraclaves.py

--- pages/ingpalabraclaves.py
+++ pages/ingpalabraclaves.py
@@ -6,4 +6,7 @@ from sqlalchemy import text
 
 
 col1, col2, = st.columns(2)
-
+if col1.button("Grabar" ,  type='primary'):
+    st.switch_page("./pages/palabrasclaves.py")
+if col2.button("Cancelar"):
+    st.switch_page("./pages/palabrasclaves.py")
