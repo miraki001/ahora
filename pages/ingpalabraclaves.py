65a37732f74088da90c6993c0421a2ae593f6fbe
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-16 12:39:29 -0300

    Update ingpalabraclaves.py

--- pages/ingpalabraclaves.py
+++ pages/ingpalabraclaves.py
@@ -8,6 +8,8 @@ if tipo == 'Editat':
     tpalabras = st.session_state['vpalabra'] 
     tpeso = st.session_state['vpeso'] 
 
+vpalabra = st.text_input("Palabra", tpalabra)
+
 col1, col2, = st.columns(2)
 if col1.button("Grabar" ,  type='primary'):
     st.switch_page("./pages/palabrasclaves.py")
