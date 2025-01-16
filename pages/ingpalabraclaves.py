dfd5f2675571aa3f3e25a747ffe287171ad568c0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-16 12:53:56 -0300

    Update ingpalabraclaves.py

--- pages/ingpalabraclaves.py
+++ pages/ingpalabraclaves.py
@@ -4,25 +4,23 @@ from sqlalchemy import text
 
 
 def actualizar:
+    conn = st.connection("postgresql", type="sql")
     with conn.session as session:
-        actualiza = "UPDATE fuentes_py SET fuente = :url, activa = :activa,"
-        actualiza = actualiza + "xpath_titulo = :tit, "
-        actualiza = actualiza + "descrip = :desc, "
-        actualiza = actualiza + "pais = :pais, "
-        actualiza = actualiza + "separador = :separador, "
-        actualiza = actualiza + "xpath_detalle = :det, "
-        actualiza = actualiza + "xpath_link = :link, "
-        actualiza = actualiza + "xpath_image = :image, "
-        actualiza = actualiza + "tipo = :tipo, "
-        actualiza = actualiza + "busqueda_pers = :busq, "
-        actualiza = actualiza + "idioma = :idioma, "
-        actualiza = actualiza + "cod_pais = :cod "
-        actualiza = actualiza + " WHERE nuri= :nuri"
-        #"desc": vtitle, "fuente": vurl, "pais": pais,"activa": activa, "separador": separador, "det": xpath_det, "link": xpath_link,"image": xpath_image, "tipo": tipo,"busq": busqueda, "idioma": idioma,"cod": codigo
-        session.execute(text(actualiza), {"url": vurl,"activa": activa,"tit": xpath_tit,"desc": vtitle, "pais": pais,"separador": separador, "det": xpath_det, "link": xpath_link,"image": xpath_image, "tipo": tipo,"busq": busqueda, "idioma": idioma,"cod": codigo, "nuri": tnuri})
-        #session.execute(text("UPDATE fuentes_py SET fuente = :url, activa = :activa, xpath_titulo = :tit    WHERE nuri= :nuri"), {"url": vurl,"activa": activa,"tit": xpath_tit, "desc": vtitle, "fuente": vurl, "pais": pais,"activa": activa, "separador": separador, "det": xpath_det, "link": xpath_link, "tipo": tipo,"busq": busqueda, "idioma": idioma,"cod": codigo,"nuri": tnuri})
-                        
+        actualiza = "UPDATE palabras_a_buscar SET palabra = :palabra"
+        actualiza = actualiza + "peso = :peso, "
+        actualiza = actualiza + " WHERE palabra= :palabra ;"
+        session.execute(text(actualiza), {"palabra": vpalabra,"peso": vpeso})
         session.commit()
+
+def ingresar:
+    conn = st.connection("postgresql", type="sql")
+    with conn.session as session:
+        actualiza = "insert into palabras_a_buscar (palabra,peso"
+        actualiza = actualiza + " values (:palabra,:peso) ;"
+        actualiza = actualiza + " WHERE palabra= :palabra"
+        session.execute(text(actualiza), {"palabra": vpalabra,"peso": vpeso})
+        session.commit()
+
 
 tipo = st.session_state['vTipo'] 
 if tipo == 'Editar':
@@ -42,6 +40,10 @@ vpeso = st.number_input("Peso", tpeso)
 
 col1, col2, = st.columns(2)
 if col1.button("Grabar" ,  type='primary'):
+    if tipo == 'Editar':
+        actualizar()
+    else
+        ingresar()
     st.switch_page("./pages/palabrasclaves.py")
 if col2.button("Cancelar"):
     st.switch_page("./pages/palabrasclaves.py")
