222450465dca620eeffe60d65fb5f4e974bbd9c0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-17 15:13:34 -0300

    Update verifpagbs.py

--- pages/verifpagbs.py
+++ pages/verifpagbs.py
@@ -59,7 +59,7 @@ if tipobusq== 'json':
     data = page_soup.select("[type='application/json']")[4]
     #data = page_soup.select(separador)[vpos]
     st.write(data)
-    ojson = json.dumps(data)
+    ojson = json.dumps(data.text)
     for product in ojson:
         st.write(product["title"])
 
