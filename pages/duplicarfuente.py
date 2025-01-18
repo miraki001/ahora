d75bca24f2956495d2a85dfb87fc6d3be5986b33
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-18 10:38:45 -0300

    Update duplicarfuente.py

--- pages/duplicarfuente.py
+++ pages/duplicarfuente.py
@@ -5,3 +5,10 @@ from sqlalchemy import text
 st.set_page_config(layout="wide")
 conn = st.connection("postgresql", type="sql")
 tnuri = st.session_state['vnuri']
+
+
+with conn.session as session:
+    actualiza = "insert into sectores (nuri,proyecto_nuri,sector,color)"
+    actualiza = actualiza + " values (nextval('sectores_seq'),:proyecto_nuri,:sector,:color) ;"
+    session.execute(text(actualiza), {"proyecto_nuri": vpro_nuri,"sector": vsector,"color": vcolor})
+    session.commit()
