235067aac279d7c78a1f69aabcaea354c13caf92
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-18 17:35:56 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -3,7 +3,11 @@ import psycopg2
 from sqlalchemy import text
 from streamlit_extras.stylable_container import stylable_container
 
-st.session_state.disabled = True
+if 'run_button' in st.session_state and st.session_state.run_button == True:
+    st.session_state.running = True
+else:
+    st.session_state.running = False
+
 
 col41, mid, col42 = st.columns([1,1,20])
 with col41:
@@ -47,7 +51,7 @@ if col1.button("Home" ,  type='primary'):
 if col2.button("Insertar"):
     st.session_state['vTipo'] = 'Ingresar'
     st.switch_page("./pages/editar_fuentes.py")
-if col3.button("Editar",disabled=st.session_state.disabled ):
+if col3.button("Editar", disabled=st.session_state.running, key='run_button'):
     st.session_state['vTipo'] = 'Editar'
     st.switch_page("./pages/editar_fuentes.py")
 if col4.button("Borrar", ):
