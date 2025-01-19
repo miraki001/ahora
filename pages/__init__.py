d03fff03513d36dff743cac24267467ed842011d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-19 13:20:14 -0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,7 +1,7 @@
-from pages.home import show_home
+from pages.home import show_editar_fuente
 from pages.editar_fuentes import show_editar_fuente
-from pages.user_guide import show_user_guide
-from pages.api import show_api
-from pages.examples import show_examples
-from pages.community import show_community
+from pages.user_guide import show_editar_fuente
+from pages.api import show_editar_fuente
+from pages.examples import show_editar_fuente
+from pages.community import show_editar_fuente
 
