74ae369b2f9a900c1f0c3608a15a1ec9effc9797
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-19 13:30:36 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -9,53 +9,7 @@ from streamlit_navigation_bar import st_navbar
 
 
 
-pages = ["Install", "User Guide", "API", "Examples", "Community", "GitHub"]
-parent_dir = os.path.dirname(os.path.abspath(__file__))
-logo_path = os.path.join(parent_dir, "ic_launcher44.pn")
-urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
-styles = {
-    "nav": {
-        "background-color": "royalblue",
-        "justify-content": "left",
-    },
-    "img": {
-        "padding-right": "14px",
-    },
-    "span": {
-        "color": "white",
-        "padding": "14px",
-    },
-    "active": {
-        "background-color": "white",
-        "color": "var(--text-color)",
-        "font-weight": "normal",
-        "padding": "14px",
-    }
-}
-options = {
-    "show_menu": False,
-    "show_sidebar": False,
-}
 
-page = st_navbar(
-    pages,
-    #logo_path=logo_path,
-    urls=urls,
-    styles=styles,
-    options=options,
-)
-
-functions = {
-    #"Home": pg.show_fuentes,
-    "Install": pg.show_fuentes,
-    "User Guide": pg.show_fuentes,
-    "API": pg.show_fuentes,
-    "Examples": pg.show_fuentes,
-    "Community": pg.show_fuentes,
-}
-go_to = functions.get(page)
-if go_to:
-    go_to()
 st.session_state.vcnt = 0
 st.session_state.cnt = 0
 cnt = 0
