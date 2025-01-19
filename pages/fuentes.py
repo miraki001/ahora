3437fc311a2759524626c789d2c03ac4927f1c62
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-19 12:24:39 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -4,6 +4,56 @@ from sqlalchemy import text
 from streamlit_extras.stylable_container import stylable_container
 from streamlit_navigation_bar import st_navbar
 
+
+
+
+pages = ["Install", "User Guide", "API", "Examples", "Community", "GitHub"]
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(parent_dir, "cubes.svg")
+urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
+styles = {
+    "nav": {
+        "background-color": "royalblue",
+        "justify-content": "left",
+    },
+    "img": {
+        "padding-right": "14px",
+    },
+    "span": {
+        "color": "white",
+        "padding": "14px",
+    },
+    "active": {
+        "background-color": "white",
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    }
+}
+options = {
+    "show_menu": False,
+    "show_sidebar": False,
+}
+
+page = st_navbar(
+    pages,
+    logo_path=logo_path,
+    urls=urls,
+    styles=styles,
+    options=options,
+)
+
+functions = {
+    "Home": pg.show_home,
+    "Install": pg.show_install,
+    "User Guide": pg.show_user_guide,
+    "API": pg.show_api,
+    "Examples": pg.show_examples,
+    "Community": pg.show_community,
+}
+go_to = functions.get(page)
+if go_to:
+    go_to()
 st.session_state.vcnt = 0
 st.session_state.cnt = 0
 cnt = 0
@@ -19,8 +69,7 @@ with col42:
     st.title('Miraki')
 
 #st.subheader("Plataforma de Vigilancia Tecnólogica e Inteligencia Competitiva")
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
-st.write(page)
+
 posjson = st.number_input("Posición del Json",min_value=0,max_value=100,value=0,key='vcnt1')
 
 
