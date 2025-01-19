a011bc6d76997b1fadebcdcce1bb52eabae99208
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-19 12:25:24 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import psycopg2
+import os
 from sqlalchemy import text
 from streamlit_extras.stylable_container import stylable_container
 from streamlit_navigation_bar import st_navbar
