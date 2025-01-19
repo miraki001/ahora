e53b3ba813843526c7d397f6bcec3eb6d3cb0a6d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-19 13:21:29 -0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,7 +1,7 @@
-from pages.home import show_editar_fuente
 from pages.editar_fuentes import show_editar_fuente
-from pages.user_guide import show_editar_fuente
-from pages.api import show_editar_fuente
-from pages.examples import show_editar_fuente
-from pages.community import show_editar_fuente
+from pages.editar_fuentes import show_editar_fuente
+from pages.editar_fuentes import show_editar_fuente
+from pages.editar_fuentes import show_editar_fuente
+from pages.editar_fuentes import show_editar_fuente
+from pages.editar_fuentes import show_editar_fuente
 
