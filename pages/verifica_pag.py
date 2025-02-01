43830b2e2b71eb64131a24ee8b8c1ef24118c664
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-01 18:09:11 -0300

    Update verifica_pag.py

--- pages/verifica_pag.py
+++ pages/verifica_pag.py
@@ -72,7 +72,7 @@ def get_driver():
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
-options.add_argument("javascript.enabled", True)
+#options.add_argument("javascript.enabled", True)
 
 driver = get_driver()
 
