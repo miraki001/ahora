4440d98929e1cc9ffa74680d66199a294bfe1d88
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-05 11:15:11 -0300

    Update verif_sele.py

--- pages/verif_sele.py
+++ pages/verif_sele.py
@@ -16,12 +16,14 @@ import os
 def get_driver():
     options = webdriver.ChromeOptions()
     service = Service()
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=service, options=options)
+    return webdriver.Chrome(service=service, options=option)
 
 
 
-url = 'https://pubs.acs.org/action/doSearch?field1=AllField&target=default&targetTab=std&text1=grape&startPage=&sortBy=Earliest'
-    
+#url = 'https://pubs.acs.org/action/doSearch?field1=AllField&target=default&targetTab=std&text1=grape&startPage=&sortBy=Earliest'
+url = 'https://www.sciencedirect.com/search/api?qs=grape&show=25&sortBy=date&t=b2bea8931965191740cebf4326a31a21a8486820cfa51ad697655fa41e8788844478bff3a9cec3d490b9736620833474ef4741e99f61f4c1feeb5fba67eb5267429071982f7331cea50cc6881d1cb5a31b09de97ae0cbfe16d3269a015400c4414cbf85106eba2be3cae9a054f2bb164&hostname=www.sciencedirect.com'
+
 browser = get_driver()
 options = Options()
 options.add_argument('--disable-gpu')
