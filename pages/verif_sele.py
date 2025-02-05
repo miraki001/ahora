034cc6ecb55cc1a2647ace77777c9d613f768080
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-05 11:41:37 -0300

    Update verif_sele.py

--- pages/verif_sele.py
+++ pages/verif_sele.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 import requests
