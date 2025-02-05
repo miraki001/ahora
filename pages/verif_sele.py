4842b2826da9f72c5396c01457c4238e3befb4e3
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-05 11:26:58 -0300

    Update verif_sele.py

--- pages/verif_sele.py
+++ pages/verif_sele.py
@@ -18,26 +18,26 @@ my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (K
 def get_driver():
     options = webdriver.ChromeOptions()
     
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-features=NetworkService")
-    options.add_argument("--window-size=1920x1080")
-    options.add_argument("--disable-features=VizDisplayCompositor")
-    options.add_argument("--enable-javascript")
+    #options.add_argument('--disable-gpu')
+    #options.add_argument('--headless')
+    #options.add_argument("--no-sandbox")
+    #options.add_argument("--disable-dev-shm-usage")
+    #options.add_argument("--disable-features=NetworkService")
+    #options.add_argument("--window-size=1920x1080")
+    #options.add_argument("--disable-features=VizDisplayCompositor")
+    #options.add_argument("--enable-javascript")
     
     #options.add_argument(f"--window-size={width}x{height}")
-    options.add_argument(f"--user-agent={my_user_agent}")
+    #options.add_argument(f"--user-agent={my_user_agent}")
     
     service = Service()
     driver = webdriver.Chrome(service=service, options=options)
     
     return webdriver.Chrome(service=service, options=options)
 
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+#options = Options()
+#options.add_argument('--disable-gpu')
+#options.add_argument('--headless')
 #options.add_argument("javascript.enabled", True)
 
 driver = get_driver()
@@ -45,14 +45,15 @@ driver = get_driver()
 url = 'https://www.sciencedirect.com/search/api?qs=grape&show=25&sortBy=date&t=b2bea8931965191740cebf4326a31a21a8486820cfa51ad697655fa41e8788844478bff3a9cec3d490b9736620833474ef4741e99f61f4c1feeb5fba67eb5267429071982f7331cea50cc6881d1cb5a31b09de97ae0cbfe16d3269a015400c4414cbf85106eba2be3cae9a054f2bb164&hostname=www.sciencedirect.com'
 
 
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+#options = Options()
+#options.add_argument('--disable-gpu')
+#options.add_argument('--headless')
 
 
 driver.get(url)
-driver.execute_script("window.scrollTo(0,10000)")
+#driver.execute_script("window.scrollTo(0,10000)")
 sleep(2)
+st.write(driver.page_source)
 soup = BeautifulSoup(driver.page_source,"html.parser")
 st.write(soup)
 
