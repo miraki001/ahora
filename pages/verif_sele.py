c245a0bf666a41c9f4f938750b03a9330e9c53b6
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-05 11:28:25 -0300

    Update verif_sele.py

--- pages/verif_sele.py
+++ pages/verif_sele.py
@@ -18,8 +18,8 @@ my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (K
 def get_driver():
     options = webdriver.ChromeOptions()
     
-    #options.add_argument('--disable-gpu')
-    #options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
     #options.add_argument("--no-sandbox")
     #options.add_argument("--disable-dev-shm-usage")
     #options.add_argument("--disable-features=NetworkService")
