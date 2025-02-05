d2ed1a2befeda2748bbd6add36e09810093f8ce5
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-05 16:29:10 -0300

    Update verif_sele.py

--- pages/verif_sele.py
+++ pages/verif_sele.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import pandas as pd
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
 
