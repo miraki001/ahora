1af2fb1906f4ec9614edcd067293a807981f5ece
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-10 16:44:18 -0300

    Update verplay.py

--- pages/verplay.py
+++ pages/verplay.py
@@ -7,6 +7,19 @@ from bs4 import BeautifulSoup
 sep = 'article'
 dictitu = {'class':'inline-post-section section-full'}
 
+browser = chromium.launch(headless=False)
+context = browser.new_context(viewport={"width": 1920, "height": 1080})
+page = context.new_page()
+
+page.goto("https://www.growingproduce.com/fruits/grapes/")  # go to url
+#page.wait_for_selector("div[data-target=directory-first-item]")  # wait for content to load
+
+parsed = []
+soup = BeautifulSoup(page.content(), 'lxml' )
+    #print('resultado')
+    #print(soup)
+noticias = soup.find_all(sep,dictitu )
+
 with sync_playwright() as pw:
     browser = pw.chromium.launch(headless=False)
     context = browser.new_context(viewport={"width": 1920, "height": 1080})
